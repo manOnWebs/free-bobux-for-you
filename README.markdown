b7194d202bf1a316982c71024255c2b3cb5c637d
manOnWebs <80761624+manOnWebs@users.noreply.github.com>
2021-07-02 01:50:31 -0500

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Free Bobux For You
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/NotYellowntnt/pen/bGgjryE](https://codepen.io/NotYellowntnt/pen/bGgjryE).
+
+FREEBOBUX
---
+++ src/script.js
@@ -0,0 +1,19 @@
+alert("AHAH, DO YOU WANT FREE BOBUX? WE ARE REAL 100% JUST CLICK ON THE BUTTON");
+
+var nomeGiocatore;
+nomeGiocatore = window.prompt("WHAT'Z YOUR NAME");
+document.getElementById("titolo").innerHTML = "FREE BOBUX" + " FOR " + nomeGiocatore
+
+var sceltaUtente;
+
+function E(){
+	sceltaUtente = "E"
+	alert("you have summoned lord farquaad" + " with E");
+}
+
+function BOBUX(){
+	if(sceltaUtente == "E"){
+		alert("Damn!, I wanted to rickroll you, but with power of E you stopped me!");
+	}else{
+		alert("Never Gonna Give You Up, Never Gonna Let You Down, Never gonna run around and desert you!")}
+	}
